cccad3ad0fec18d2caf2ea8341d0645678be8ce9
Quantum <quantum2048@gmail.com>
2015-12-11 16:01:54 -0500

    Declare 1.9 compatibility.

--- wpadmin/__init__.py
+++ wpadmin/__init__.py
@@ -3,7 +3,7 @@ django_wpadmin is a collection of extensions/tools for the default Django
 administration panel which makes it look and behave more like WordPress
 administration panel.
 """
-VERSION = (1, 8, 7)
+VERSION = (1, 9, 0)
 
 try:
     from django import VERSION as DJANGO_VERSION
